681b13c65acefe036764b5252093812b0dbb78b8
Ryan <payat1284@gmail.com>
2026-09-18 13:46:23 +0800

    Fix CommandBus reentrant dispatch deadlock

--- LumaLive/shared/common/CommandBus.hpp
+++ LumaLive/shared/common/CommandBus.hpp
@@ -16,7 +16,7 @@ class CommandBus final: public ICommandBus {
 public:
 contracts::Result Register(const std::string& n,CommandHandler h) override {std::scoped_lock l(m_); handlers_[n]=std::move(h); return contracts::Result::Ok();}
 contracts::Result Unregister(const std::string& n) override {std::scoped_lock l(m_); handlers_.erase(n); return contracts::Result::Ok();}
-contracts::Result Dispatch(const contracts::commands::Command& c) override {std::scoped_lock l(m_); auto it=handlers_.find(c.id); if(it==handlers_.end()) return contracts::Result::Failure(contracts::ErrorCode::NotImplemented); return it->second(c);}
+contracts::Result Dispatch(const contracts::commands::Command& c) override {CommandHandler h; {std::scoped_lock l(m_); auto it=handlers_.find(c.id); if(it==handlers_.end()) return contracts::Result::Failure(contracts::ErrorCode::NotImplemented); h=it->second;} return h(c);}
 private: std::mutex m_; std::unordered_map<std::string,CommandHandler> handlers_;
 };
 }
