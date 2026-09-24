74f85e88a18d4139bd8dfa839e8c55ca0d2d4e49
Ryan <payat1284@gmail.com>
2026-09-24 18:17:59 +0800

    refactor(auth): remove file-backed auth store API

--- LumaLive/server/auth/include/IAuthService.hpp
+++ LumaLive/server/auth/include/IAuthService.hpp
@@ -3,9 +3,10 @@
 #include <memory>
 #include <string>
 #include "contracts/errors/Error.hpp"
-#include "ISmsProvider.hpp"
 
 namespace luma::server::auth {
+class IAuthStore;
+class ISmsProvider;
 class IAuthService {
 public:
     virtual ~IAuthService() = default;
@@ -14,8 +15,7 @@ public:
     virtual shared::contracts::Result Stop() = 0;
     virtual bool IsRunning() const = 0;
     virtual std::uint16_t Port() const = 0;
-    virtual shared::contracts::Result ConfigureStore(std::string path) = 0;
-    virtual shared::contracts::Result ConfigureSmsProvider(std::unique_ptr<ISmsProvider> provider) = 0;
+    virtual shared::contracts::Result ConfigureDatabase(std::string connection_string) = 0;
 };
-std::unique_ptr<IAuthService> CreateAuthService();
+std::unique_ptr<IAuthService> CreateAuthService(std::unique_ptr<IAuthStore> store = {});
 }
